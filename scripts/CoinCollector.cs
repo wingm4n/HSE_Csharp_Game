0f8d30ce2663063d8cf2db18b656ad0fd13be0cc
Zlata Tyurina <zdtyurina@edu.hse.ru>
2026-03-14 21:41:17 +0300

    TEST

--- scripts/CoinCollector.cs
+++ scripts/CoinCollector.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 
 // Author: Tyurina Z.
 
@@ -8,18 +8,18 @@
 public partial class CoinCollector : Node
 {
 	[Export] public Godot.Label _coinLabel;
-    [Export] public Godot.Label _gameEnd;
-    [Export] public Button _gameRestart;
-    private int coins = 0;
+	[Export] public Godot.Label _gameEnd;
+	[Export] public Button _gameRestart;
+	private int coins = 10;
 
 	public override void _Ready()
 	{
 		if (_coinLabel != null)
 			_coinLabel.Text = "Coins: 0";
-        _gameEnd = GetTree().Root.FindChild("GameEnd", true, false) as Godot.Label;
-        _gameRestart = GetTree().Root.FindChild("GameRestart", true, false) as Button;
-        _coinLabel = GetTree().Root.FindChild("CoinLabel", true, false) as Godot.Label;
-    }
+		_gameEnd = GetTree().Root.FindChild("GameEnd", true, false) as Godot.Label;
+		_gameRestart = GetTree().Root.FindChild("GameRestart", true, false) as Button;
+		_coinLabel = GetTree().Root.FindChild("CoinLabel", true, false) as Godot.Label;
+	}
 
 	public void AddCoin()
 	{
