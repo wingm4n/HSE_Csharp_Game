914980957a3403e6bdbe9e45044e2a346c57824c
wingm4n <alexkorostt@gmail.com>
2026-03-22 18:07:04 +0300

    Label bug fixed

--- Label.cs
+++ Label.cs
@@ -14,7 +14,10 @@ public partial class Label : Godot.Label
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	
-		Text = $"HP {_player.Health}";}
+		if (_player != null)
+		{
+			Text = $"HP {_player.Health}";
+		}
+	}
 	
 }
